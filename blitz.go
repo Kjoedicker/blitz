0bcf31d1f9a43da3acdbdff5f86ba817ac87c711
Kjoedicker <kjoedicker@gmail.com>
2023-10-09 19:08:18 -0400

    feat(blitz): implement `makeSynchronizedRequests` and `makeRequestsForDuration`

---
+++ blitz.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func trackRequestCount() func() int {
+	totalRequests := 0
+	return func() int {
+		totalRequests++
+		return totalRequests
+	}
+}
+
+func makeSynchronizedRequests(requestStructure Request, hitsPerSecond int) {
+	queue := sync.WaitGroup{}
+
+	requests := make(Requests, hitsPerSecond)
+	returnRequestNumber := trackRequestCount()
+
+	for totalRequests := 1; totalRequests <= hitsPerSecond; totalRequests++ {
+		queue.Add(1)
+
+		go func() {
+			request := requestStructure
+			request.Number = returnRequestNumber()
+
+			request.Call()
+
+			requests[request.Number-1] = request
+
+			queue.Done()
+		}()
+	}
+
+	queue.Wait()
+
+	requests.PrintResults()
+
+	time.Sleep(time.Second)
+}
+
+func makeRequestsForDuration(requestStructure Request, duration int, hitsPerSecond int) {
+
+	done := make(TimedChannel)
+	go done.After(time.Duration(duration) * time.Minute)
+
+	for requestGroup := 0; ; requestGroup++ {
+		request := requestStructure
+		request.RequestGroup = requestGroup
+
+		select {
+		case <-done:
+			return
+		default:
+			makeSynchronizedRequests(request, hitsPerSecond)
+		}
+	}
+}
