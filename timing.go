e0a15819755e3b24e8419c182f6f3e0d9d6e5d1b
kjoedicker <kjoedicker@gmail.com>
2023-10-08 22:36:05 -0400

    feat(timing) rename timer -> timing and export `HitsPer` and `MinutesToSeconds`

--- timing.go
+++ timing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 	"time"
 )
 
@@ -11,4 +12,12 @@ func timer(identifier int) func() {
 		seconds := time.Since(start).Seconds()
 		log.Printf("Call %d took: %fs \n", identifier, seconds)
 	}
+}
+
+func HitsPer(totalRequests int, interval int) (HitsPer int) {
+	return int(math.Round(float64(totalRequests) / float64(interval)))
+}
+
+func MinutesToSeconds(minutes int) int {
+	return minutes * 60
 }
