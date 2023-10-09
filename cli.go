e4194ce18b11e27f4d0b8bc40b9ee6ffa7340b9b
Kjoedicker <kjoedicker@gmail.com>
2023-10-09 19:58:34 -0400

    refactor(cli, main): move `printLogo` into the `main` file

--- cli.go
+++ cli.go
@@ -2,31 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
 )
 
 const (
 	DEFAULT_FILE_PATH = "./test-plan.yml"
 )
 
-func printLogo() {
-	blitzLogo := `
-▄▄▄▄   ██▓    ██▄▄▄█████▒███████▒
-▓█████▄▓██▒   ▓██▓  ██▒ ▓▒ ▒ ▒ ▄▀░
-▒██▒ ▄█▒██░   ▒██▒ ▓██░ ▒░ ▒ ▄▀▒░ 
-▒██░█▀ ▒██░   ░██░ ▓██▓ ░  ▄▀▒   ░
-░▓█  ▀█░██████░██░ ▒██▒ ░▒███████▒
-░▒▓███▀░ ▒░▓  ░▓   ▒ ░░  ░▒▒ ▓░▒░▒
-▒░▒   ░░ ░ ▒  ░▒ ░   ░   ░░▒ ▒ ░ ▒
- ░    ░  ░ ░   ▒ ░ ░     ░ ░ ░ ░ ░
- ░         ░  ░░           ░ ░    
-	  ░                  ░        
-`
-
-	fmt.Println(blitzLogo)
-}
-
-func parseTestPlanPath() (testPlanFilePath string) {
+func ParseTestPlanPath() (testPlanFilePath string) {
 	testPlanFilePathPtr := flag.String("test-plan-path", DEFAULT_FILE_PATH, "Test plan path. Defaults to: "+DEFAULT_FILE_PATH)
 
 	flag.Parse()
--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
+import "fmt"
+
+func printLogo() {
+	blitzLogo := `
+▄▄▄▄   ██▓    ██▄▄▄█████▒███████▒
+▓█████▄▓██▒   ▓██▓  ██▒ ▓▒ ▒ ▒ ▄▀░
+▒██▒ ▄█▒██░   ▒██▒ ▓██░ ▒░ ▒ ▄▀▒░ 
+▒██░█▀ ▒██░   ░██░ ▓██▓ ░  ▄▀▒   ░
+░▓█  ▀█░██████░██░ ▒██▒ ░▒███████▒
+░▒▓███▀░ ▒░▓  ░▓   ▒ ░░  ░▒▒ ▓░▒░▒
+▒░▒   ░░ ░ ▒  ░▒ ░   ░   ░░▒ ▒ ░ ▒
+ ░    ░  ░ ░   ▒ ░ ░     ░ ░ ░ ░ ░
+ ░         ░  ░░           ░ ░    
+	  ░                  ░        
+`
+
+	fmt.Println(blitzLogo)
+}
+
 func main() {
 	printLogo()
 
-	testPlanPath := parseTestPlanPath()
-	testPlan := loadTestPlan(testPlanPath)
+	testPlanPath := ParseTestPlanPath()
+	testPlan := LoadTestPlan(testPlanPath)
 
 	testPlan.begin()
 }
