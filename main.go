de6f4187e3faf0645e40803af7e7abb82bb63e07
Kjoedicker <keifer.joedicker@merkle.com>
2024-07-03 21:04:18 -0400

    feat(main.go): update `main` function to take a more functional approach to parsing, building and running request plans

--- main.go
+++ main.go
@@ -1,29 +1,19 @@
 package main
 
-import "fmt"
-
-func printLogo() {
-	blitzLogo := `
-▄▄▄▄   ██▓    ██▄▄▄█████▒███████▒
-▓█████▄▓██▒   ▓██▓  ██▒ ▓▒ ▒ ▒ ▄▀░
-▒██▒ ▄█▒██░   ▒██▒ ▓██░ ▒░ ▒ ▄▀▒░ 
-▒██░█▀ ▒██░   ░██░ ▓██▓ ░  ▄▀▒   ░
-░▓█  ▀█░██████░██░ ▒██▒ ░▒███████▒
-░▒▓███▀░ ▒░▓  ░▓   ▒ ░░  ░▒▒ ▓░▒░▒
-▒░▒   ░░ ░ ▒  ░▒ ░   ░   ░░▒ ▒ ░ ▒
- ░    ░  ░ ░   ▒ ░ ░     ░ ░ ░ ░ ░
- ░         ░  ░░           ░ ░    
-	  ░                  ░        
-`
-
-	fmt.Println(blitzLogo)
-}
+import (
+	"github.com/Kjoedicker/blitz/cli"
+	"github.com/Kjoedicker/blitz/logo"
+	"github.com/Kjoedicker/blitz/plan"
+	"github.com/Kjoedicker/blitz/request"
+)
 
 func main() {
-	printLogo()
+	logo.Print()
+	planFilePath := cli.ParseTestPlanPath()
+	testPlan := plan.Load(planFilePath)
+	requests := request.BuildRequests(testPlan)
 
-	testPlanPath := ParseTestPlanPath()
-	testPlan := LoadTestPlan(testPlanPath)
-
-	testPlan.Begin()
+	for _, requestPrototype := range requests {
+		Execute(requestPrototype)
+	}
 }
